22700a6a157bc278897666e165ccb92281298827
John Doe <johndoe@example.com>
2022-08-29 14:46:42 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import {Landing} from './component/landing';
+import React from 'react';
+import useNavigate from 'react-router-dom';
 
 function App() {
+
+  let navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Landing/>
+
+      <button className="border-2" onClick={() => navigate('/myprofile')}>Go to my profile</button>
     </div>
+    
   );
 }
 
---
+++ src/component/myprofile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export const Profile = () => {
+
+     return(
+        <div>
+            <div>My name is Aida</div> 
+         </div>
+     )
+} 
+
