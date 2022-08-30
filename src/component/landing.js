90b68f64969d22e51dd34020382f9516af96a7a4
nadiaaida <johndoe@example.com>
2022-08-30 08:55:17 +0800

    3 commit

--- src/component/landing.js
+++ src/component/landing.js
@@ -6,6 +6,7 @@ const navigate =useNavigate();
     return(
         <div>
             <p>Landing Page page baru</p>
+            <p>aida nadia</p>
             <button className="border-2" onClick = {() => navigate('/myprofile')}>Go to my profile</button>
         </div>
     )
